adcbe1fa119e310e2853972646e00752a849c845
linnypham <89488749+linnypham@users.noreply.github.com>
2024-09-10 11:40:05 -0400

    bj

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from blackjack import blackjack
 
-player = [10,11]
-dealer = 7
+a = int(input('Input your first hand: '))
+b = int(input('Input your second hand: '))
+c = int(input('Input dealer hand: '))
+
+player = [a,b]
+dealer = c
 print(blackjack(player,dealer))
