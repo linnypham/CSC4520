faa49da61ffa575cb2ad61f681c683df63269dd3
linnypham <89488749+linnypham@users.noreply.github.com>
2024-09-11 11:42:00 -0400

    recursive

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-from blackjack import blackjack
-
-a = int(input('Input your first hand: '))
-b = int(input('Input your second hand: '))
-c = int(input('Input dealer hand: '))
-
-player = [a,b]
-dealer = c
-print(blackjack(player,dealer))
+from recursive import function
+print(function(3))
--- recursive.py
+++ recursive.py
@@ -2,3 +2,13 @@ def power(x,n):
     if n == 0:
         return 1
     return x*power(x,n-1)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+def function(n):
+    if n == 0:
+        return 1
+    return function(n/3) + 1
