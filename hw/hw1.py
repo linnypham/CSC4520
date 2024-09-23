5d13d8ef0d148e3730d34de954da5b5aaa56115c
linnypham <89488749+linnypham@users.noreply.github.com>
2024-09-23 14:31:45 -0400

    hw

--- hw/hw1.py
+++ hw/hw1.py
@@ -6,11 +6,11 @@ def problem_4(A):
             min_val = A[i]
         if A[i]>max_val:
             max_val = A[i]
-    return (max_val - min_val)
+    return max_val - min_val
 def problem_5(n):
     if n == 1:
         return n
     return problem_5(n-1)+n**3
 
-n = 5
+n = 2
 print(problem_5(n))
