0ff034b22d31701a2104fcd3e63daf3eb63372a7
linnypham <89488749+linnypham@users.noreply.github.com>
2024-12-03 01:21:28 -0500

    Create hash.py

---
+++ hash.py
@@ -0,0 +1,21 @@
+def openHashing(arr,nb):
+    open_arr = {i:[] for i in range(nb)}
+    for num in arr:
+        loc = num%nb
+        open_arr[loc].append(num)
+    return open_arr
+def closeHashing(arr,nb):
+    close_arr = [None]*nb
+    for num in arr:
+        loc = num%nb
+        for i in range(loc,nb):
+            if close_arr[i] == None:
+                close_arr[i] = num
+                break
+    return close_arr
+
+
+input = [63, 42, 45, 64, 53, 41]
+print(closeHashing(input,11))
+            
+        
